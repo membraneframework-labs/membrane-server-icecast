dc342db80120c0dafafc5be89e7fdf1d22023a86
Dominik Stanaszek <dominikstanaszek@icloud.com>
2019-05-21 10:07:45 +0200

    Refer to protocol project and create a test suite

---
+++ lib/membrane_server_icecast/output/protocol.ex
@@ -0,0 +1,17 @@
+defmodule Membrane.Server.Icecast.Output.Protocol do
+  alias Membrane.Server.Icecast.Output.Machine
+
+  @behaviour :ranch_protocol
+
+  @impl true
+  def start_link(ref, _, transport, {controller_module, controller_arg, server_string, request_timeout, body_timeout}) do
+    {:ok, :proc_lib.spawn_link(__MODULE__, :init, [ref, transport, controller_module, controller_arg, server_string, request_timeout, body_timeout])}
+  end
+
+  @doc false
+  def init(ref, transport, controller_module, controller_arg, server_string, request_timeout, body_timeout) do
+    {:ok, socket} = :ranch.handshake(ref)
+    machine_module = Application.get_env(:membrane_server_icecast, :output_machine)
+    machine_module.init({socket, transport, controller_module, controller_arg, server_string, request_timeout, body_timeout})
+  end
+end
